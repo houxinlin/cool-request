a8d82f9b43781375772b72b5245f5ceb1981ca6f
houxinlin <houxinlin@seczone.cn>
2023-12-01 18:03:19 +0800

    add model

---
+++ src/main/java/com/hxl/plugin/springboot/invoke/invoke/InvokeTimeoutException.java
@@ -0,0 +1,4 @@
+package com.hxl.plugin.springboot.invoke.invoke;
+
+public class InvokeTimeoutException  extends InvokeException{
+}
---
+++ src/main/java/com/hxl/plugin/springboot/invoke/model/InvokeReceiveModel.java
@@ -0,0 +1,13 @@
+package com.hxl.plugin.springboot.invoke.model;
+
+public class InvokeReceiveModel {
+    private String requestId;
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+}
