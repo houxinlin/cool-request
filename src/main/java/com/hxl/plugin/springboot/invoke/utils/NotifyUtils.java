060fdd2f16bdc1c6929b314a261ac0c28bc6a93e
hxl <2606710413@qq.com>
2023-12-03 09:47:39 +0800

    update ApifoxConfigurable

--- src/main/java/com/hxl/plugin/springboot/invoke/utils/NotifyUtils.java
+++ src/main/java/com/hxl/plugin/springboot/invoke/utils/NotifyUtils.java
@@ -1,17 +1,15 @@
 package com.hxl.plugin.springboot.invoke.utils;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.NotificationType;
+import com.intellij.notification.*;
+import com.intellij.notification.impl.NotificationGroupEP;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.MessageType;
 
 public class NotifyUtils {
-    private static final NotificationGroup NOTIFICATION_GROUP = new NotificationGroup("com.hxl.plugin.scheduled-invoke", NotificationDisplayType.BALLOON, true);
 
     public static void notification(Project project, String msg) {
-        final Notification notification = NOTIFICATION_GROUP.createNotification(msg, NotificationType.INFORMATION);
-        notification.notify(project);
+        NotificationGroup myCustomNotificationGroup = NotificationGroupManager.getInstance().getNotificationGroup("CoolRequestNotificationGroup");
+        myCustomNotificationGroup.createNotification(msg, MessageType.INFO).notify(project);
     }
 }
--- src/main/kotlin/invoke/dsl/ApifoxConfigurable.kt
+++ src/main/kotlin/invoke/dsl/ApifoxConfigurable.kt
@@ -3,9 +3,11 @@ package invoke.dsl
 import com.hxl.plugin.springboot.invoke.plugin.apifox.ApiFoxExport
 import com.hxl.plugin.springboot.invoke.plugin.apifox.ApiFoxExportCondition
 import com.hxl.plugin.springboot.invoke.state.SettingPersistentState
+import com.intellij.openapi.observable.properties.ObservableMutableProperty
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
-import com.intellij.ui.layout.panel
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.dsl.builder.bindText
 import javax.swing.JComponent
 import javax.swing.JLabel
 import kotlin.concurrent.thread
@@ -20,26 +22,34 @@ class ApifoxConfigurable(val project: Project) : Configurable {
     private val apiExport: ApiFoxExport = ApiFoxExport(project)
     private lateinit var authorLabel: JLabel
     private lateinit var openLabel: JLabel
+    private lateinit var authorText: JBTextField
+    private lateinit var openText: JBTextField
     override fun createComponent(): JComponent {
         authorizationTextFieldProperty = (state.apiFoxAuthorization)
         openApiTextFieldProperty = (state.openApiToken)
-        return panel {
+        return com.intellij.ui.dsl.builder.panel {
             row("HTTP Authorization") {
-                textField(::authorizationTextFieldProperty,{authorizationTextFieldProperty=it})
+                textField().bindText(::authorizationTextFieldProperty) {
+                    authorizationTextFieldProperty = it
+                }.component.apply {
+                    authorText = this
+                }
                 label("").component.apply { authorLabel = this }
             }
 
             row("OpenApi Token") {
-                textField(::openApiTextFieldProperty,{openApiTextFieldProperty=it})
+                textField().bindText(::openApiTextFieldProperty) { openApiTextFieldProperty = it }.component.apply {
+                    openText = this
+                }
                 label("").component.apply { openLabel = this }
             }
             row {
                 button("Check") {
-                    state.apiFoxAuthorization = authorizationTextFieldProperty
-                    state.openApiToken = openApiTextFieldProperty
+                    state.apiFoxAuthorization = authorText.text
+                    state.openApiToken = openText.text
                     thread {
                         val apiFoxExportCondition =
-                            ApiFoxExportCondition(authorizationTextFieldProperty, openApiTextFieldProperty)
+                            ApiFoxExportCondition(authorText.text, openText.text)
                         val checkToken = apiExport.checkToken(apiFoxExportCondition)
                         authorLabel.text = (
                                 if (checkToken.getOrDefault(ApiFoxExportCondition.KEY_API_FOX_AUTHORIZATION, false)) {
@@ -59,6 +69,7 @@ class ApifoxConfigurable(val project: Project) : Configurable {
                 }
             }
         }
+
     }
 
     override fun isModified(): Boolean {
