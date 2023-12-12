3e09b0b05fff4120cfb721aa9544aff0b91d9fe1
hxl <2606710413@qq.com>
2023-12-12 21:32:35 +0800

    update script api

---
+++ src/main/java/com/hxl/plugin/springboot/invoke/utils/FileUtils.java
@@ -0,0 +1,28 @@
+package com.hxl.plugin.springboot.invoke.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileUtils {
+    public static boolean writeFile(String target, byte[] content) {
+        Path path = Paths.get(target);
+        if (Files.notExists(path.getParent())) {
+            try {
+                Files.createDirectories(path.getParent());
+            } catch (IOException ignored) {
+            }
+        }
+        try {
+            Files.write(path, content);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+    public  static boolean writeFile(String target, String content) {
+        if (content == null) return false;
+        return writeFile(target, content.getBytes());
+    }
+}
