ad328afadb3454ba95d693b05ae7605fb25a7ee2
houxinlin <houxinlin@seczone.cn>
2023-12-15 10:45:22 +0800

    fix

--- jni/com_hxl_plugin_springboot_invoke_utils_file_NativeWindowDialogUtils.c
+++ jni/com_hxl_plugin_springboot_invoke_utils_file_NativeWindowDialogUtils.c
@@ -1,24 +1,6 @@
 //#include <com_hxl_plugin_springboot_invoke_utils_file_NativeWindowDialogUtils.h>
 //#include <windows.h>
 //
-//jstring WindowsTojstring( JNIEnv* env, char* str )
-//{
-//    jstring rtn = 0;
-//    int slen = strlen(str);
-//    unsigned short * buffer = 0;
-//    if( slen == 0 )
-//        rtn = (*env)->NewStringUTF(env,str );
-//    else
-//    {
-//        int length = MultiByteToWideChar( CP_ACP, 0, (LPCSTR)str, slen, NULL, 0 );
-//        buffer = (unsigned short *)malloc( length*2 + 1 );
-//        if( MultiByteToWideChar( CP_ACP, 0, (LPCSTR)str, slen, (LPWSTR)buffer, length ) >0 )
-//            rtn = (*env)->NewString(env,  (jchar*)buffer, length );
-//    }
-//    if( buffer )
-//        free( buffer );
-//    return rtn;
-//}
 //
 //JNIEXPORT jstring JNICALL Java_com_hxl_plugin_springboot_invoke_utils_file_NativeWindowDialogUtils_openFileSelectDialog
 //  (JNIEnv *env, jclass obj) {
@@ -35,9 +17,7 @@
 //    ofn.lpstrTitle = titleA;
 //    ofn.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST;
 //    if (GetOpenFileNameA(&ofn)) {
-//        char out[MAX_PATH] = "";
-////        WideCharToMultiByte(CP_ACP, 0, L"\u9009\u62e9\u6587\u4ef6", -1, out, MAX_PATH, NULL, NULL);
-//        return WindowsTojstring(env,"啊");
+
 //    }
 //    return NULL;
 //}
