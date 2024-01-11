ce256f18d7ae0ccb0cc4393c4b15ac7afc428c00
houxinlin <houxinlin@seczone.cn>
2024-01-11 09:30:44 +0800

    删除无用文件

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation(files("/home/LinuxWork/project/java/openapi-generator/build/libs/openapi-generator-1.0-SNAPSHOT.jar"))
-//    implementation(files("D:\\project\\java\\openapi-generator\\build\\libs\\openapi-generator-1.0-SNAPSHOT.jar"))
+//    implementation(files("/home/LinuxWork/project/java/openapi-generator/build/libs/openapi-generator-1.0-SNAPSHOT.jar"))
+    implementation(files("D:\\project\\java\\openapi-generator\\build\\libs\\openapi-generator-1.0-SNAPSHOT.jar"))
 }
 
 // Configure Gradle IntelliJ Plugin
