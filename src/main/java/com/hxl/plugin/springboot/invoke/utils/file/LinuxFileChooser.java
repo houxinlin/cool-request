ab8e62ec4ce16b62b90362dc6a5f902e73c0a688
hxl <2606710413@qq.com>
2024-01-13 00:50:01 +0800

    merge

---
+++ src/main/java/com/hxl/plugin/springboot/invoke/utils/file/LinuxFileChooser.java
@@ -0,0 +1,28 @@
+package com.hxl.plugin.springboot.invoke.utils.file;
+
+import com.intellij.openapi.project.Project;
+
+/**
+ * todo: to be implemented
+ *
+ * @author zhangpengjun
+ * @date 2024/1/12
+ */
+public class LinuxFileChooser extends BasicFileChooser {
+
+    @Override
+    public String chooseSingleFile(String basePath, String fileName, Project project) {
+        return null;
+    }
+
+    @Override
+    public String chooseDirector(Project project) {
+        return null;
+    }
+
+    @Override
+    public String chooseFileSavePath(String basePath, String fileName, Project project) {
+        return null;
+    }
+
+}
