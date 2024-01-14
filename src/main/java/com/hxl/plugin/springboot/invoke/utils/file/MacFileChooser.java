973a9ded6b06735b4eab00f24f1a032ee7cf202f
zpj80231 <zpj80231@163.com>
2024-01-14 17:18:04 +0800

    feat: Add mac file selection implementation

--- src/main/java/com/hxl/plugin/springboot/invoke/utils/file/MacFileChooser.java
+++ src/main/java/com/hxl/plugin/springboot/invoke/utils/file/MacFileChooser.java
@@ -2,9 +2,12 @@ package com.hxl.plugin.springboot.invoke.utils.file;
 
 import com.intellij.openapi.project.Project;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
 /**
- * todo: to be implemented
- *
  * @author zhangpengjun
  * @date 2024/1/12
  */
@@ -12,16 +15,47 @@ public class MacFileChooser extends BasicFileChooser {
 
     @Override
     public String chooseSingleFile(String basePath, String fileName, Project project) {
-        return null;
+        return chooseFileInternal(basePath, fileName, false);
     }
 
     @Override
     public String chooseDirector(Project project) {
-        return null;
+        return chooseFileInternal(null, null, true);
     }
 
     @Override
     public String chooseFileSavePath(String basePath, String fileName, Project project) {
+        return chooseFileInternal(basePath, fileName, false);
+    }
+
+    private String chooseFileInternal(String basePath, String fileName, boolean isDirectory) {
+        String script = isDirectory ? "choose folder" : "choose file";
+        if (basePath != null) {
+            script += " default location \"" + basePath + "\"";
+        }
+        if (fileName != null) {
+            script += " default name \"" + fileName + "\"";
+        }
+
+        String result = executeAppleScript(script);
+        if (result != null) {
+            return Paths.get(result).toString();
+        }
+
+        return null;
+    }
+
+    private String executeAppleScript(String script) {
+        try {
+            Process process = Runtime.getRuntime().exec(new String[]{"osascript", "-e", script});
+            process.waitFor();
+            if (process.exitValue() == 0) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                return reader.readLine();
+            }
+        } catch (IOException | InterruptedException ignored) {
+        }
+
         return null;
     }
 
