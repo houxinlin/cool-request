4ccb9364f3e86992e51be6ebdae6a7215c4d5e90
zpj80231 <zpj80231@163.com>
2024-01-26 19:33:23 +0800

    feat: 修改controller method图标快捷键，单击定位导航，双击快速发起请求

--- src/main/java/com/cool/request/action/nav/RestRequestNavHandler.java
+++ src/main/java/com/cool/request/action/nav/RestRequestNavHandler.java
@@ -1,13 +1,22 @@
 package com.cool.request.action.nav;
 
 import com.cool.request.utils.NavigationUtils;
+import com.cool.request.view.component.ApiToolPage;
+import com.cool.request.view.component.MainBottomHTTPContainer;
+import com.cool.request.view.main.MainTopTreeView;
+import com.cool.request.view.tool.ProviderManager;
+import com.cool.request.view.tool.ToolActionPageSwitcher;
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
+import com.intellij.ui.treeStructure.Tree;
+import com.intellij.util.ui.tree.TreeUtil;
 
 import javax.swing.*;
+import javax.swing.tree.TreePath;
 import java.awt.event.MouseEvent;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author zhangpj
@@ -19,8 +28,30 @@ public class RestRequestNavHandler implements GutterIconNavigationHandler<PsiEle
     public void navigate(MouseEvent e, PsiElement elt) {
         Project project = elt.getProject();
         PsiMethod method = (PsiMethod) elt.getParent();
+        // 单击导航
         if (SwingUtilities.isLeftMouseButton(e)) {
             NavigationUtils.jumpToNavigation(project, method);
+            AtomicReference<TreePath> selectedPathIfOne = new AtomicReference<>();
+            ProviderManager.findAndConsumerProvider(MainTopTreeView.class, project, mainTopTreeView -> {
+                Tree tree = mainTopTreeView.getTree();
+                selectedPathIfOne.set(TreeUtil.getSelectedPathIfOne(tree));
+            });
+            ProviderManager.findAndConsumerProvider(ToolActionPageSwitcher.class, project, toolActionPageSwitcher -> {
+                toolActionPageSwitcher.goToByName(ApiToolPage.PAGE_NAME,
+                        selectedPathIfOne.get().getLastPathComponent());
+            });
+        }
+        // 双击发起请求
+        if (e.getClickCount() == 2 && SwingUtilities.isLeftMouseButton(e)) {
+            AtomicReference<TreePath> selectedPathIfOne = new AtomicReference<>();
+            ProviderManager.findAndConsumerProvider(MainTopTreeView.class, project, mainTopTreeView -> {
+                Tree tree = mainTopTreeView.getTree();
+                selectedPathIfOne.set(TreeUtil.getSelectedPathIfOne(tree));
+            });
+            ProviderManager.findAndConsumerProvider(ToolActionPageSwitcher.class, project, toolActionPageSwitcher -> {
+                toolActionPageSwitcher.goToByName(MainBottomHTTPContainer.PAGE_NAME,
+                        selectedPathIfOne.get().getLastPathComponent());
+            });
         }
     }
 
