5d80edfc3d13b9c94207fc222e2fee5954c0ec66
houxinlin <houxinlin@seczone.cn>
2024-02-05 16:34:47 +0800

    修改构建版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.6.20"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
     id("org.jetbrains.intellij") version "1.10.1"
-    id("io.sentry.jvm.gradle") version "4.2.0"
 }
 
 group = "com.hxl.plugin"
-version = "2024.2.15"
+version = ""
 
 repositories {
     maven { url = uri("https://maven.aliyun.com/repository/public/") }
@@ -14,13 +13,14 @@ repositories {
 }
 dependencies {
     implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.85")
-
     implementation("org.apache.mina:mina-core:2.2.3")
+    implementation("com.google.code.gson:gson:2.9.0")
     implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.22.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation(files("deps/openapi-generator-1.0-SNAPSHOT.jar"))
     implementation(files("deps/cool-request-script-api-1.0-SNAPSHOT.jar"))
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+
 }
 
 // Configure Gradle IntelliJ Plugin
@@ -32,26 +32,18 @@ intellij {
     updateSinceUntilBuild.set(false)
 
 }
-sentry  {
-    // Generates a JVM (Java, Kotlin, etc.) source bundle and uploads your source code to Sentry.
-    // This enables source context, allowing you to see your source
-    // code as part of your stack traces in Sentry.
-    includeSourceContext.set(true)
-
-    org.set("tingfengshiye")
-    projectName.set("java")
-    authToken.set("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")
-}
 
 tasks {
-    // Set the JVM compatibility versions
-
     withType<JavaCompile> {
         sourceCompatibility = "11"
         targetCompatibility = "11"
         options.encoding = "UTF-8"
     }
-
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
     patchPluginXml {
         sinceBuild.set("203")
     }
