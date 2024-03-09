5c1cbce628099212b32c0bb8d042f44503d5eb63
houxinlin <2606710413@qq.com>
2024-03-09 16:09:06 +0800

    feat:显示查找快捷键文本

--- src/main/java/com/cool/request/action/actions/FindAction.java
+++ src/main/java/com/cool/request/action/actions/FindAction.java
@@ -1,11 +1,16 @@
 package com.cool.request.action.actions;
 
 import com.cool.request.common.icons.KotlinCoolRequestIcons;
+import com.cool.request.common.state.SettingPersistentState;
 import com.cool.request.utils.ResourceBundleUtils;
 import com.cool.request.view.tool.search.ApiAbstractGotoSEContributor;
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.keymap.KeymapUtil;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 /**
  * @author caoayu
@@ -15,6 +20,16 @@ public class FindAction extends DynamicAnAction {
         super(project, () -> ResourceBundleUtils.getString("find"),
                 () -> ResourceBundleUtils.getString("find"),
                 KotlinCoolRequestIcons.INSTANCE.getSEARCH());
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        int searchApiKeyCode = SettingPersistentState.getInstance().getState().searchApiKeyCode;
+        int searchApiModifiers = SettingPersistentState.getInstance().getState().searchApiModifiers;
+        KeyStroke keyStroke = KeyStroke.getKeyStroke(searchApiKeyCode, searchApiModifiers, false);
+        e.getPresentation().setText(KeymapUtil.getKeystrokeText(keyStroke));
+        getTemplatePresentation().setText(KeymapUtil.getKeystrokeText(keyStroke));
     }
 
     @Override
