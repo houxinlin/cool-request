55a614578a97229035e02703ce8ef5baa19fd978
houxinlin <2606710413@qq.com>
2024-03-09 15:52:17 +0800

    fix:恢复AboutAnAction

--- src/main/java/com/cool/request/action/actions/AboutAnAction.java
+++ src/main/java/com/cool/request/action/actions/AboutAnAction.java
@@ -1,18 +1,10 @@
 package com.cool.request.action.actions;
 
 import com.cool.request.common.icons.CoolRequestIcons;
+import com.cool.request.view.dialog.AboutDialog;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.keymap.KeymapUtil;
-import com.intellij.openapi.keymap.impl.ui.KeymapPanel;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.popup.JBPopup;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.ui.components.JBList;
-import com.intellij.ui.components.fields.ExpandableTextField;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AboutAnAction extends BaseAnAction {
     public AboutAnAction(Project project) {
@@ -21,24 +13,7 @@ public class AboutAnAction extends BaseAnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        List<String> suggestions = new ArrayList<>();
-
-
-        suggestions.add("Option 1");
-        suggestions.add("Option 2");
-        suggestions.add("Option 3");
-        JBList<String> suggestionList = new JBList<>(suggestions.toArray(new String[0]));
-        JBPopup popup = JBPopupFactory.getInstance()
-                .createListPopupBuilder(suggestionList)
-                .setTitle("Suggestions")
-                .setItemChoosenCallback(() -> {
-                    String selected = suggestionList.getSelectedValue();
-                    if (selected != null) {
-                    }
-                })
-                .createPopup();
-        popup.showUnderneathOf(e.getInputEvent().getComponent());
-//        new AboutDialog(getProject()).show();
+        new AboutDialog(getProject()).show();
     }
 
 }
