b99a8e8eca5e292dc9ed3c6ef3b18dc57d86c884
houxinlin <2606710413@qq.com>
2024-03-27 18:21:31 +0800

    增加协议

--- src/main/java/com/cool/request/common/cache/CacheStorageServiceImpl.java
+++ src/main/java/com/cool/request/common/cache/CacheStorageServiceImpl.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2024 XIN LIN HOU<hxl49508@gmail.com>
+ * CacheStorageServiceImpl.java is part of Cool Request
+ *
+ * License: GPL-3.0+
+ *
+ * Cool Request is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cool Request is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cool Request.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.cool.request.common.cache;
 
 import com.cool.request.common.constant.CoolRequestConfigConstant;
--- src/main/java/com/cool/request/utils/Assert.java
+++ src/main/java/com/cool/request/utils/Assert.java
@@ -1,21 +1,24 @@
-package com.cool.request.utils;
-
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2024 XIN LIN HOU<hxl49508@gmail.com>
+ * Assert.java is part of Cool Request
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * License: GPL-3.0+
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ * Cool Request is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Cool Request is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cool Request.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+package com.cool.request.utils;
 
 import java.util.Collection;
 import java.util.Map;
